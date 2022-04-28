6a033cb260959abf9c0c82158940bfcd8079f6a8
portiapalakitti <90639971+portiapalakitti@users.noreply.github.com>
2022-04-28 12:24:54 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('.screenshots-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav: true,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+           
+        },
+        600:{
+            items:3,
+            
+        },
+        1000:{
+            items:4,
+            
+        }
+    }
+});
+
